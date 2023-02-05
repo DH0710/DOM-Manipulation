f139b9ec52cb7fd792ba7599ea725aa438ddf381
blur <damienhebert710@gmail.com>
2023-02-05 15:10:47 -0600

    building tiles by creating random index to choose from colorsPickList, witihin a for loop

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,18 @@ const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "
 const colorsPickList = colors.concat(colors);
 const tileCount = colorsPickList.length;
 
+// Game State
+
+let revealedCount = 0;
+let activeTile = null; // refers to the tile the user just clicked on
+let awaitngEndOfMove = false; 
+
+
+
+// Build up TIles
+
+for (let i = 0; i < tileCount; i++) {
+const randomIndex = Math.floor(Math.random() * colorsPickList.length);
+const color = colorsPickList[randomIndex];
+
+}
