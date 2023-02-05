dc48557f848388ef61c37d951cc60b2eae0c2335
blur <damienhebert710@gmail.com>
2023-02-05 15:03:21 -0600

    adding constants to main.js

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+const tileContainer = document.querySelector(".tiles");
+const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
+const colorsPickList = colors.concat(colors);
+const tileCount = colorsPickList.length;
+
