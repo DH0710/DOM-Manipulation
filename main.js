27c6f12ee645872ac0d0bcd8d5211e8b0effa941
blur <damienhebert710@gmail.com>
2023-02-09 21:02:33 -0600

    dawg im barely awake or aware to know what i did

--- main.js
+++ main.js
@@ -9,6 +9,21 @@ let revealedCount = 0;
 let activeTile = null; // refers to the tile the user just clicked on
 let awaitngEndOfMove = false; 
 
+// builds a new tile element then return it 
+buildTile = (color) => {
+const element = document.createElement("div");
+
+element.classList.add("tile");
+element.setAttribute("data-color", color);
+
+return element;
+
+
+
+}
+
+
+
 
 
 // Build up TIles
@@ -16,5 +31,12 @@ let awaitngEndOfMove = false;
 for (let i = 0; i < tileCount; i++) {
 const randomIndex = Math.floor(Math.random() * colorsPickList.length);
 const color = colorsPickList[randomIndex];
+
+const tile = buildTile(color);
+
+colorsPickList.splice(randomIndex,1); // will make sure  
+// the randomized array doesnt choose the same color more than 2 times
+console.log(color);
+document.body.appendChild(tile);
 
 }
